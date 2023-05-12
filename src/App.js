d43456e0f327cbd592cc83cb89282a4e6a6a8032
tcjardine <112444700+tcjardine@users.noreply.github.com>
2023-05-12 09:17:07 -0700

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://steady-stroopwafel-59b96d.netlify.app/index.html"
+          target="_blank"
+        >
+          Tracy Jardine
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/tcjardine/react-weather-app"
+          target="_blank"
+        >
+          opened-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
