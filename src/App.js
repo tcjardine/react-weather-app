f5ede4b0704b0c8b01843e56cafa7c8f48f1cc1f
tcjardine <112444700+tcjardine@users.noreply.github.com>
2023-05-12 10:49:29 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://steady-stroopwafel-59b96d.netlify.app/index.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Tracy Jardine
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/tcjardine/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          opened-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://steady-stroopwafel-59b96d.netlify.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tracy Jardine
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/tcjardine/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            opened-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
